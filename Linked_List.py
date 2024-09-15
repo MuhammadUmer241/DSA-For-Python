d4b60948d8dad85439c292f11ef41a57bf3e6ad6
Muhammad Umer <umernadeem241@gmail.com>
2024-09-15 13:56:29 -0700

    dsa

---
+++ Linked_List.py
@@ -0,0 +1,167 @@
+class Node:
+    """
+    Blue print for a Node
+    """
+    def __init__(self,value):
+        self.data= value # value of current node
+        self.next= None # address of last node
+
+
+class linked_list:
+    """
+    contains methods used in linked list to insert delete , Find etc
+    """
+    def __init__(self):
+        self.head =None
+        self.n= 0
+
+
+
+    def insert_head(self,value:any)->None:
+        """
+        method to add node by head
+        :param value:
+        :return: None
+        """
+        new_node= Node(value) #initialize new node
+        new_node.next= self.head #contains address of last node
+        self.head= new_node
+        self.n= self.n +1
+        print(new_node.next, self.head)
+
+
+    def insert_tail(self,value:any)->None:
+        """
+        method to insert node by tail
+
+        :param value:
+        :return:
+        """
+        new_node= Node(value)
+        if self.head == None:
+            self.head= new_node
+            self.n= self.n + 1
+            return
+        curr = self.head
+        while curr.next!= None:
+            curr= curr.next
+
+        curr.next= new_node
+        self.n = self.n +1
+
+
+
+    def insert_after(self, after:any, value:any)->None:
+        """
+        Method to insert value after a certain value
+        :param after:
+        :param value:
+        :return:
+        """
+        new_node= Node(value)
+        curr = self.head
+
+        while curr.data!= after:
+            curr= curr.next
+            if curr.next.next==None:
+                # if we are going to insert value after tail
+                break
+
+        new_node.next= curr.next
+        curr.next= new_node
+        self.n = self.n +1
+
+
+
+    def delete_head(self)->None:
+        """
+        delete the head
+        :return: None
+        """
+        if self.head==None:
+            # Our list is empty
+            print("list is empty")
+
+
+        self.head= self.head.next
+        self.n = self.n -1
+
+    def delete_tail(self)->None:
+        """
+        Delete Tail of the Node
+        :return:
+        """
+        curr= self.head
+
+        if curr.next == None:
+            # if this block is executed it means we have only one node in it and we are going to delete it
+            return  self.delete_head() # already created func to delete the head
+
+        while curr.next.next != None:
+            curr= curr.next
+
+        curr.next= None
+        self.n= self.n-1
+
+    def delete_after(self,after:any)->None:
+        """
+        deleter the value
+        :param after:
+        :return:
+        """
+        curr= self.head
+
+        while curr.data!=after:
+            curr = curr.next
+            if curr.next.next==None: # case to handle if value is tail
+                break
+
+
+        curr.next= curr.next.next
+        self.n = self.n-1
+
+    def find(self, value)->None:
+        """
+        print index of given value
+        :param value:
+        :return: None
+        """
+        pos= 0
+        curr= self.head
+        # print(curr.next)
+        while curr != None:
+            if curr.data==value:
+                 return print("Item on", pos)
+            curr = curr.next
+            pos = pos + 1
+        return print("Index Not found")
+
+
+    def __str__(self)->str:
+        curr = self.head
+        result = ''
+
+
+        while curr != None:
+            result = result + str(curr.data) + "->"
+            curr = curr.next
+
+
+        return result[:-2]
+
+obj = linked_list()
+obj.insert_head(1)
+print(obj)
+obj.insert_tail(3)
+obj.insert_after(1,4)
+obj.insert_after(4,5)
+print(obj)
+obj.insert_after(6,6)
+print("Node after insertion Test",obj)
+obj.delete_after(3)
+print("Node after deletion Test",obj)
+obj.find(9)
+obj.find(4)
+print(obj)
+
+
---
+++ dynamic_arrays.py
@@ -0,0 +1,130 @@
+import ctypes
+
+class dynamic_array:
+    def __init__(self):
+        """
+        dynamic_array is class to handle problems cause by array , as we know array have fixed shape which can
+        be an issue in order to dealing with  values dynamically increasing or decreasing
+        """
+        self.n= 0 # number of chr
+        self.size= 1 # size/len of array
+        self.A= self.__create_array(self.size)
+
+    def __str__(self)->str:
+        """
+        method to return array in desired shape
+        :return: str
+        """
+        result = ""
+        for i in range(self.n):
+            result= result+ str(self.A[i]) + ","
+        return "["+result[:-1]+"]"
+
+    def __delitem__(self, key:int)->None:
+        """
+        delete the value on given index
+        :param key:
+        :return:
+        """
+
+        self.__index(key)
+        for i in range(key,self.n-1):
+            self.A[i] = self.A[i+1]
+
+        self.n= self.n-1
+
+    def insert(self,pos:int, item:any )->None:
+        """
+        Insert item on specified position and push value the old value one step ahead
+        :param pos:
+        :param item:
+        :return: None
+        """
+        self.__index(pos) # private method to raise error if number of items is greater then the position provided
+        if self.n == self.size:
+            self.__resize_array(2 * self.size)
+
+        for i in range(self.n, pos, -1):
+            self.A[i]= self.A[i-1] # creating a position according to "pos" to append item there
+
+        self.A[pos]= item
+        self.n= self.n+1
+
+
+    def __index(self, pos:int)->None:
+        """
+        raise an error when
+        :param pos:
+        :return: None
+        """
+        if self.n<pos:
+            raise "index-out-of-range"
+    def __len__(self):
+        """
+        number of elements in array
+        :return: None
+        """
+        print(self.n)
+
+
+
+    def pop(self)->None:
+        """
+        Pop the last item
+        :return: None
+        """
+        if self.n==0:
+            print("Empty List")
+        else:
+            print(self.A[:self.n-1])
+            self.n=self.n-1
+
+    def append(self,item:any)->None:
+        """
+        append item to the last
+        :param item:
+        :return: None
+        """
+        if self.size== self.n:
+            self.__resize_array(self.size*2)
+        self.A[self.n]= item  # append value to last index
+        self.n = self.n + 1
+
+
+    def __resize_array(self,size:int)->None:
+        """
+        create an array B to extend the size of array A by replacing B by A
+        :param size:
+        :return: None
+        """
+        B= self.__create_array(size)
+        self.size= size
+        for values in range(self.n):
+            B[values]= self.A[values]
+        self.A= B
+
+    def __create_array(self, size):
+        """
+        using a C library to create a C array
+        :param size:
+        :return: array of given size
+        """
+        return (size * ctypes.py_object)()
+
+
+
+
+
+my_list= dynamic_array() #making instance
+print("Empty ", my_list)
+my_list.append("Muhammad") #append string
+my_list.append(2) # append int
+my_list.append(True) #append bol
+print("Append ", my_list)
+my_list.insert(3,"Pakistan") #insert value
+print("Insert ", my_list)
+my_list.__delitem__(1)
+print("Del ", my_list)
+my_list.pop()
+
+print("pop",my_list)
