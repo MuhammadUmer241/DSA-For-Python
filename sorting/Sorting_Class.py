aa99b5f692101addcd0af14c9f7d3314324cd1be
Muhammad Umer <umernadeem241@gmail.com>
2024-09-15 15:02:59 -0700

    Bubble Sort completion

--- sorting/Sorting_Class.py
+++ sorting/Sorting_Class.py
@@ -1,17 +1,24 @@
 
-class sorting:
+class Sorting:
     def __init__(self,array:list[int]):
-        self.array= array
+        self.array= array # array to sort
 
     def bubble_sort(self)->list[int]:
         """
-        Sorting the array by bubble sort
-        :param array:
+        Bubble Sort is a simple sorting algorithm that repeatedly
+         compares adjacent elements in an array and swaps them if
+         they are in the wrong order. This process continues until
+         the array is fully sorted. It's a relatively inefficient
+         algorithm, especially for large datasets,
+         but it's easy to understand and implement
+
         :return: sorted array
         """
+
         n= len(self.array)
-        for i in range(n):
-            for index in range(n-1):
+        for i in range(n): # Iterates according to the length of list
+            for index in range(n-1): # Compares adjacent elements and swaps them if needed
+                # comparing and swapping if needed
                 if self.array[index]>self.array[index+1]:
                     self.array[index], self.array[index+1]= self.array[index+1],self.array[index]
 
@@ -21,5 +28,5 @@ class sorting:
 
 
 
-obj= sorting([23,13,29,19,21])
+obj= Sorting([23,13,29,19,21])
 print(obj.bubble_sort())
